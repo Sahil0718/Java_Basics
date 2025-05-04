e7b7e9152842ab42920378512c1114387b3157ac
Sahil0718 <135214197+Sahil0718@users.noreply.github.com>
2025-05-04 15:15:13 +0545

    done up to nested loops

---
+++ src/Loops.java
@@ -0,0 +1,12 @@
+public class Loops {
+    //code that runs multiple times based on a condition. Repeated execution of codes.
+    //loop automates repetitive tasks
+    //Types of loops: while,for,do-while
+    public static void main(String[] args) {
+        int num = 1;
+        while (num <= 5) {
+            System.out.println(num);
+            num++;
+        }
+    }
+}
